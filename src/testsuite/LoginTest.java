4ea01e6ab33714a58c1c30158f78a7360cca9ace
Palakchothani <palakchothani11@gmail.com>
2022-11-25 09:45:01 +0000

    project4-saucedemo-sw-palak

---
+++ src/testsuite/LoginTest.java
@@ -0,0 +1,63 @@
+package testsuite;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.Utility;
+
+public class LoginTest extends Utility
+{
+    String basUrl ="https://www.saucedemo.com/";
+
+    @Before
+    public void setUp(){
+        openBrowser(basUrl);
+
+    }
+    @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials(){
+        // enter   username
+        sendTextToElement(By.name("user-name"),"standard_user");
+
+        // enter password
+        sendTextToElement(By.id("password"),"secret_sauce");
+
+        // click on login button
+        clickOnElement(By.xpath("//input[@type='submit']"));
+
+
+        String expectMessage= "PRODUCTS";
+
+        //Find the login button and click on it
+
+        WebElement actualTextMessageElement = driver.findElement(By.xpath("//span[@class='title']"));
+        String actualMessage = actualTextMessageElement.getText();
+
+
+        //validate actual and expected message
+        Assert.assertEquals("",expectMessage,actualMessage);
+    }
+
+    @Test
+    public void  verifyThatSixProductsAreDisplayedOnPage(){
+
+        // enter   username
+        sendTextToElement(By.name("user-name"),"standard_user");
+
+        // enter password
+        sendTextToElement(By.id("password"),"secret_sauce");
+
+        // click on login button
+        clickOnElement(By.xpath("//input[@type='submit']"));
+
+        boolean isInventoryDisplayed = driver.findElement(By.id("inventory_container")).isDisplayed();
+        System.out.println("all six item displayed" +isInventoryDisplayed);
+    }
+    @After
+    public void TearDown(){
+        //   closeBrowser();
+    }
+}
